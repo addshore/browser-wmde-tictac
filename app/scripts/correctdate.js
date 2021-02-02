2512e71f373686cde8a488598e23049be95b7e3d
addshore <addshorewiki@gmail.com>
2021-02-02 09:49:51 +0000

    WIP still ...

--- app/scripts/correctdate.js
+++ app/scripts/correctdate.js
@@ -1,49 +1,64 @@
-var perSheetInterval = null
-var appWideInterval = setInterval(function() {
-  if (document.querySelector('input.oe_datepicker_container')) {
-    clearInterval(appWideInterval);
+var intervals = [];
+
+var generalInterval = setInterval(function() {
+  // Wait for the loading element to set an observer on
+  if (document.querySelector('.oe_loading')) {
+    clearInterval(generalInterval);
     onSheetLoad()
      var observer = new MutationObserver(function () {
       onSheetLoad()
     })
+    // And load every time the loading element changes
     observer.observe(document.querySelector('.oe_loading'), { attributes: true })
   }
 }, 100); // check every 100ms
 
 function onSheetLoad() {
-  if(perSheetInterval !== null) {
-    // Clear interval from pervious sheet if there was one
-    clearInterval(perSheetInterval);
-  }
+  clearIntervals()
 
-  perSheetInterval = setInterval(function() {
+  var newInterval = setInterval(function() {
     // Keep looking for the date input fields to be in the DOM (only appear in editing mode)
-    if (document.querySelector('input.oe_datepicker_container')) {
-      clearInterval(perSheetInterval);
-
-      var observer = new MutationObserver(function () {
-        if(document.querySelector('.oe_datepicker_root')){
-          injectCorrectDateJs()
-        }
-      })
-      observer.observe(document.querySelector('input.oe_datepicker_container').parentElement.parentElement, { attributes: true })
-      injectCorrectDateJs()
-
+    var datepickerDom = document.querySelector('input.oe_datepicker_container')
+    if (datepickerDom) {
+      clearIntervals()
+      // Only create an observer for a date picker that doesn't already have one
+      if(!datepickerDom.hasAttribute("data-is-observed-by-correctdate")){
+        var observer = new MutationObserver(function () {
+          console.log("observed inner")
+          if(document.querySelector('input.oe_datepicker_container')){
+            console.log("inject called")
+            injectCorrectDateJs()
+          }
+        })
+        datepickerDom.setAttribute("data-is-observed-by-correctdate","1")
+        observer.observe(document.querySelector('.oe_list_editable'), { attributes: true })
+        injectCorrectDateJs()
+      }
     }
+    intervals.push(newInterval)
   }, 200); // check every 200ms
 }
 
+function clearIntervals() {
+  intervals.forEach(function (item, index, object) {
+    clearInterval(item);
+    object.splice(index, 1);
+  })
+}
+
 function injectCorrectDateJs() {
-  var node = document.querySelector('input.oe_datepicker_container')
-  var jqueryId = node.getAttribute('id')
   // The code needs access to jquery, so inject it as a snippet into the DOM
   var codeToInject = `// From correctdate.js
-var jqueryId = "${ jqueryId }"
+var node = document.querySelector('input.oe_datepicker_container')
+var jqueryId = node.getAttribute('id')
 var jqueryNode = $("input[id='"  + jqueryId +"']")
 var sheetDate = document.querySelector('[data-field="sheet_date"]')
-if(sheetDate && jqueryNode.val() !== sheetDate.textContent) {
+// If we have a sheetDate, and the current date looks different to the sheetDate
+console.log(jqueryNode.val())
+if(sheetDate && (new Date(jqueryNode.val())).getTime() != (new Date(sheetDate.textContent)).getTime()) {
+// Set the date to the sheetDate
 jqueryNode.datepicker("show")
-jqueryNode.datepicker("setDate",new Date(sheetDate))
+jqueryNode.datepicker("setDate",new Date(sheetDate.textContent))
 jqueryNode.datepicker("hide")
 }`;
 
